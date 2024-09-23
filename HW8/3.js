9f7685783fdc44d8aa4d21ae9cab375bee3a7470
Ihor Myroshnychenko <ihor.myroshnychenko@redoo-ukraine.com.ua>
2024-09-23 10:51:04 +0300

    HW 8

--- HW8/3.js
+++ HW8/3.js
@@ -3,9 +3,20 @@ function analyzeArray(arr) {
     // додаємо перетворення для забезпечення правильного результату якщо в параметрах цифри вказані як string
     const sum = numArr.reduce((acc, num) => acc + num, 0);
     const average = sum / numArr.length;
-    const min = Math.min(...numArr);
-    const max = Math.max(...numArr);
+
+    let min = numArr[0];
+    let max = numArr[0];
+
+    for (let i = 1; i < numArr.length; i++) {
+        if (numArr[i] < min) {
+            min = numArr[i];
+        }
+        if (numArr[i] > max) {
+            max = numArr[i];
+        }
+    }
+
     return { sum, average, min, max };
 }
 
-console.log(analyzeArray(['1', '2', '4', '3']));
+console.log(analyzeArray([1, 2, 4, 3]));
