2cc89f22f0c9ba3a4e512ef0dbe98dcb65eaf9b1
IhorMyroshnyckenkohm <141821875+IhorMyroshnyckenkohm@users.noreply.github.com>
2024-10-07 19:34:23 +0300

    Update 3.js

--- HW11/3.js
+++ HW11/3.js
@@ -1,15 +1,21 @@
 function retry(fn, maxAttempts) {
-  return function (...args) {
-    let result;
-    for (let attempt = 0; attempt < maxAttempts; attempt++) {
-      result = fn(...args);
+  let attempts = 0;
+  
+  return function(...args) {
+    if (attempts >= maxAttempts) {
+      throw new Error('Maximum number of attempts reached');
     }
-    return result;
+    
+    attempts++;
+    return fn(...args);
   };
 }
-
 
 const exampleFn = (x) => x * 2;
 
 const retriedFn = retry(exampleFn, 3);
-console.log(retriedFn(5)); 
+
+console.log(retriedFn(2)); // 4
+console.log(retriedFn(3)); // 6
+console.log(retriedFn(4)); // 8
+console.log(retriedFn(5)); // Error: Maximum number of attempts reached
