a1ea2afb6988a42d9b9ed17a67f2e87ee9c4b3d0
IhorMyroshnyckenkohm <141821875+IhorMyroshnyckenkohm@users.noreply.github.com>
2024-10-26 14:20:01 +0300

    Update 1,2,3.js

--- HW14.1/1,2,3.js
+++ HW14.1/1,2,3.js
@@ -1,104 +1,101 @@
-class Student {
-    constructor(firstName, lastName, birthYear, initialCourse) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthYear = birthYear;
-        this.courses = {};
-        this.addCourse(initialCourse);
-    }
+function Student(firstName, lastName, birthYear, initialCourse) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.courses = {};
 
-    addCourse(courseName) {
-        if (!this.courses[courseName]) {
-            this.courses[courseName] = {
-                grades: [],
-                attendance: [],
-            };
-        }
-    }
+    this.addCourse(initialCourse);
+}
 
-    removeCourse(courseName) {
-        delete this.courses[courseName];
-    }
-
-    addGrade(courseName, grade) {
-        if (this.courses[courseName]) {
-            this.courses[courseName].grades.push(grade);
-        }
-    }
-
-    addAttendance(courseName, attended) {
-        if (this.courses[courseName]) {
-            this.courses[courseName].attendance.push(attended);
-        }
-    }
-
-    getAverageGrade(courseName) {
-        const grades = this.courses[courseName]?.grades || [];
-        const sum = grades.reduce((a, b) => a + b, 0);
-        return grades.length ? sum / grades.length : 0;
-    }
-
-    getAverageAttendance(courseName) {
-        const attendance = this.courses[courseName]?.attendance || [];
-        const attended = attendance.filter(a => a).length;
-        return attendance.length ? attended / attendance.length : 0;
-    }
-
-    getTotalClasses(courseName) {
-        return this.courses[courseName]?.attendance.length || 0;
-    }
-
-    getCoursesInfo() {
-        return Object.entries(this.courses).map(([name, data]) => ({
-            courseName: name,
-            averageGrade: this.getAverageGrade(name),
-            averageAttendance: this.getAverageAttendance(name),
-            totalClasses: this.getTotalClasses(name),
-        }));
-    }
-
-    getInfo() {
-        return {
-            fullName: `${this.firstName} ${this.lastName}`,
-            birthYear: this.birthYear,
-            courses: this.getCoursesInfo(),
+Student.prototype.addCourse = function(courseName) {
+    if (!this.courses[courseName]) {
+        this.courses[courseName] = {
+            grades: [],
+            attendance: [],
         };
     }
+};
+
+Student.prototype.removeCourse = function(courseName) {
+    delete this.courses[courseName];
+};
+
+Student.prototype.addGrade = function(courseName, grade) {
+    if (this.courses[courseName]) {
+        this.courses[courseName].grades.push(grade);
+    }
+};
+
+Student.prototype.addAttendance = function(courseName, attended) {
+    if (this.courses[courseName]) {
+        this.courses[courseName].attendance.push(attended);
+    }
+};
+
+Student.prototype.getAverageGrade = function(courseName) {
+    const grades = this.courses[courseName]?.grades || [];
+    const sum = grades.reduce((a, b) => a + b, 0);
+    return grades.length ? sum / grades.length : 0;
+};
+
+Student.prototype.getAverageAttendance = function(courseName) {
+    const attendance = this.courses[courseName]?.attendance || [];
+    const attended = attendance.filter(a => a).length;
+    return attendance.length ? attended / attendance.length : 0;
+};
+
+Student.prototype.getTotalClasses = function(courseName) {
+    return this.courses[courseName]?.attendance.length || 0;
+};
+
+Student.prototype.getCoursesInfo = function() {
+    return Object.entries(this.courses).map(([name, data]) => ({
+        courseName: name,
+        averageGrade: this.getAverageGrade(name),
+        averageAttendance: this.getAverageAttendance(name),
+        totalClasses: this.getTotalClasses(name),
+    }));
+};
+
+Student.prototype.getInfo = function() {
+    return {
+        fullName: `${this.firstName} ${this.lastName}`,
+        birthYear: this.birthYear,
+        courses: this.getCoursesInfo(),
+    };
+};
+
+function Group() {
+    this.students = [];
 }
 
-class Group {
-    constructor() {
-        this.students = [];
-    }
+Group.prototype.addStudent = function(student) {
+    this.students.push(student);
+};
 
-    addStudent(student) {
-        this.students.push(student);
-    }
+Group.prototype.removeStudent = function(firstName, lastName) {
+    this.students = this.students.filter(
+        student => student.firstName !== firstName || student.lastName !== lastName
+    );
+};
 
-    removeStudent(firstName, lastName) {
-        this.students = this.students.filter(
-            student => student.firstName !== firstName || student.lastName !== lastName
-        );
-    }
+Group.prototype.getAttendanceRating = function(courseName) {
+    return this.students
+        .map(student => ({
+            student: `${student.firstName} ${student.lastName}`,
+            averageAttendance: student.getAverageAttendance(courseName),
+        }))
+        .sort((a, b) => b.averageAttendance - a.averageAttendance);
+};
 
-    getAttendanceRating(courseName) {
-        return this.students
-            .map(student => ({
-                student: `${student.firstName} ${student.lastName}`,
-                averageAttendance: student.getAverageAttendance(courseName),
-            }))
-            .sort((a, b) => b.averageAttendance - a.averageAttendance);
-    }
-
-    getGradeRating(courseName) {
-        return this.students
-            .map(student => ({
-                student: `${student.firstName} ${student.lastName}`,
-                averageGrade: student.getAverageGrade(courseName),
-            }))
-            .sort((a, b) => b.averageGrade - a.averageGrade);
-    }
-}
+Group.prototype.getGradeRating = function(courseName) {
+    return this.students
+        .map(student => ({
+            student: `${student.firstName} ${student.lastName}`,
+            averageGrade: student.getAverageGrade(courseName),
+        }))
+        .sort((a, b) => b.averageGrade - a.averageGrade);
+};
 
 
 const student1 = new Student("Іван", "Іваненко", 2001, "Математика");
@@ -117,7 +114,5 @@ group.addStudent(student1);
 group.addStudent(student2);
 
 console.log(group.getAttendanceRating("Математика"));
-
 console.log(group.getGradeRating("Фізика"));
-
 console.log(student1.getInfo());
